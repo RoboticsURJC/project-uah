fb16d38bc9563ba7a365c10d4ecad7f0ca9f7a9d
Javier Martinez <javimartinezmma@gmail.com>
2022-10-31 14:54:52 +0100

    updating camera app (mp4 support)

--- src/camera-app/app.py
+++ src/camera-app/app.py
@@ -9,7 +9,7 @@ import sys, os
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
-from picamera2.outputs import FileOutput
+from picamera2.outputs import FileOutput, FfmpegOutput
 from picamera2.previews.qt import QGlPicamera2
 
 basedir = os.path.dirname(__file__)
@@ -22,15 +22,17 @@ app = QApplication([])
 def play_stop_button_pressed():
     global recording
     if recording:
-        picam2.stop_encoder()
+        picam2.stop_recording()
+        picam2.start()
         window.play_stop_button.setText("Start recording")
         recording = False
     else:
-        path, extension = QFileDialog.getSaveFileName(window, 'Save File', "", ".h264")
+        picam2.stop()
+        path, extension = QFileDialog.getSaveFileName(window, 'Save File', "", ".mp4")
         if path != '':
-            encoder = H264Encoder(10000000)
-            encoder.output = FileOutput(path+extension)
-            picam2.start_encoder(encoder)
+            encoder = H264Encoder()
+            output = FfmpegOutput(path+extension, audio=False)
+            picam2.start_recording(encoder, output)
             window.play_stop_button.setText("Stop recording")
             recording = True
 
@@ -38,10 +40,9 @@ window = QWidget()
 uic.loadUi(os.path.join(basedir, 'resource/camera_controls.ui'), window)
 window.setWindowTitle("Camera App")
 window.play_stop_button.clicked.connect(play_stop_button_pressed)
-qpicamera2 = QGlPicamera2(picam2, width=640, height=480, keep_ar=True)
+qpicamera2 = QGlPicamera2(picam2, width=1640, height=1232, keep_ar=True)
 window.MainFrame.layout().addWidget(qpicamera2, 0,0,1,3)
 recording = False
-    
 
 picam2.start()
 window.show()
