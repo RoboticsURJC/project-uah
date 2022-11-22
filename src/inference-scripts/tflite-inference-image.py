5547e29b26a713ef251e133a56d48870c72368c8
Javier Martinez <javimartinezmma@gmail.com>
2022-11-22 14:11:10 +0100

    updating tflite inference scripts

--- src/inference-scripts/tflite-inference-image.py
+++ src/inference-scripts/tflite-inference-image.py
@@ -1,16 +1,51 @@
-# This code is based in: https://github.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/blob/master/TFLite_detection_image.py
-
 import cv2
 import numpy as np
 from tflite_runtime.interpreter import Interpreter
+from utils import utils
 
-MODEL_PATH = "../tensorflow-workspace/tflite/models/ssdmobilenetv2-fpnlite-320/model-v4.tflite"
+# Paths to files
+MODEL_PATH = "../tensorflow-workspace/tflite/models-with-metadata/ssdmobilenetv2-fpnlite-320/model-v4.tflite"
 TEST_IMAGE_PATH = "../tensorflow-workspace/data/data_to_test/images/0001.png"
 LABELMAP_PATH = "../tensorflow-workspace/data/label-map.txt"
 
+# Parameter values of input image in MobileNet
+INPUT_MEAN = 127.5
+INPUT_STD = 127.5
+
+# Minimun value of confidence threshold
 MIN_CONF_THRESHOLD = 0.5
 
-# Load the label map
+def preprocess_image(image_path, size):
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    resized_image = cv2.resize(image_rgb, size)
+    resized_image = np.expand_dims(resized_image, axis=0)
+    normalized_image = (np.float32(resized_image) - INPUT_MEAN) / INPUT_STD
+    return image, normalized_image
+
+def detect(interpreter, image, min_threshold):
+    tensor_index = interpreter.get_input_details()[0]['index']
+    interpreter.set_tensor(tensor_index, image)
+    interpreter.invoke()
+
+    output_details = interpreter.get_output_details()
+    scores = interpreter.get_tensor(output_details[0]['index'])[0]
+    boxes = interpreter.get_tensor(output_details[1]['index'])[0]
+    count = int(interpreter.get_tensor(output_details[2]['index'])[0])
+    classes = interpreter.get_tensor(output_details[3]['index'])[0]
+
+    results = []
+    for i in range(count):
+        if scores[i] >= min_threshold:
+            result = {
+                'bounding_box': boxes[i],
+                'class_id': classes[i],
+                'score': scores[i]
+            }
+            results.append(result)
+    return results
+
+# Load labels from label-map
 with open(LABELMAP_PATH, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
 
@@ -18,63 +53,18 @@ with open(LABELMAP_PATH, 'r') as f:
 interpreter = Interpreter(model_path=MODEL_PATH)
 interpreter.allocate_tensors()
 
-# Get model details
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+# Get input shape required by the model
+_, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
 
-height = input_details[0]['shape'][1]
-width = input_details[0]['shape'][2]
-floating_model = (input_details[0]['dtype'] == np.float32)
+# Preprocess image
+original_image, processed_image = preprocess_image(TEST_IMAGE_PATH, (input_height, input_width))
 
-input_mean = 127.5
-input_std = 127.5
-boxes_idx, classes_idx, scores_idx = 1, 3, 0
+# Run object detection
+results = detect(interpreter, processed_image, MIN_CONF_THRESHOLD)
 
-# Load image and resize to expected shape [1xHxWx3]
-image = cv2.imread(TEST_IMAGE_PATH)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-imH, imW, _ = image.shape
-image_resized = cv2.resize(image_rgb, (width, height))
-input_data = np.expand_dims(image_resized, axis=0)
+# Draw bounding boxes
+original_image = utils.visualize(original_image, results, labels)
 
-# Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-if floating_model:
-    input_data = (np.float32(input_data) - input_mean) / input_std
-
-# Perform the actual detection by running the model with the image as input
-interpreter.set_tensor(input_details[0]['index'],input_data)
-interpreter.invoke()
-
-# Bounding box coordinates of detected objects
-boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]
-# Class index of detected objects
-classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]
-# Confidence of detected objects
-scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]
-
-# Loop over all detections and draw detection box 
-# if confidence is above minimum threshold
-for i in range(len(scores)):
-    if ((scores[i] > MIN_CONF_THRESHOLD) and (scores[i] <= 1.0)):
-
-        # Get bounding box coordinates and draw box
-        # Interpreter can return coordinates that are outside of image dimensions,
-        # need to force them to be within image using max() and min()
-        ymin = int(max(1,(boxes[i][0] * imH)))
-        xmin = int(max(1,(boxes[i][1] * imW)))
-        ymax = int(min(imH,(boxes[i][2] * imH)))
-        xmax = int(min(imW,(boxes[i][3] * imW)))
-        
-        cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
-
-        # Draw label
-        object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-        label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-        label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-        cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-        cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
-
-cv2.imshow('Test image', image)
+cv2.imshow('Test image', original_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- src/inference-scripts/tflite-inference-video.py
+++ src/inference-scripts/tflite-inference-video.py
@@ -1,16 +1,50 @@
-# This code is based in: https://github.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/blob/master/TFLite_detection_video.py
-
 import cv2
 import numpy as np
 from tflite_runtime.interpreter import Interpreter
 from utils.fps import FPS
+from utils import utils
 import time
 
+# Paths to files
 MODEL_PATH = "../tensorflow-workspace/tflite/models/ssdmobilenetv2-fpnlite-320/model-v4.tflite"
 TEST_VIDEO_PATH = "../tensorflow-workspace/data/data_to_test/videos/test.mp4"
 LABELMAP_PATH = "../tensorflow-workspace/data/label-map.txt"
 
+# Parameter values of input image in MobileNet
+INPUT_MEAN = 127.5
+INPUT_STD = 127.5
+
+# Minimun value of confidence threshold
 MIN_CONF_THRESHOLD = 0.5
+
+def preprocess_frame(frame, size):
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    resized_frame = cv2.resize(frame_rgb, size)
+    resized_frame = np.expand_dims(resized_frame, axis=0)
+    normalized_frame = (np.float32(resized_frame) - INPUT_MEAN) / INPUT_STD
+    return normalized_frame
+
+def detect(interpreter, frame, min_threshold):
+    tensor_index = interpreter.get_input_details()[0]['index']
+    interpreter.set_tensor(tensor_index, frame)
+    interpreter.invoke()
+
+    output_details = interpreter.get_output_details()
+    scores = interpreter.get_tensor(output_details[0]['index'])[0]
+    boxes = interpreter.get_tensor(output_details[1]['index'])[0]
+    count = int(interpreter.get_tensor(output_details[2]['index'])[0])
+    classes = interpreter.get_tensor(output_details[3]['index'])[0]
+
+    results = []
+    for i in range(count):
+        if scores[i] >= min_threshold:
+            result = {
+                'bounding_box': boxes[i],
+                'class_id': classes[i],
+                'score': scores[i]
+            }
+            results.append(result)
+    return results
 
 # Load the label map
 with open(LABELMAP_PATH, 'r') as f:
@@ -20,71 +54,28 @@ with open(LABELMAP_PATH, 'r') as f:
 interpreter = Interpreter(model_path=MODEL_PATH)
 interpreter.allocate_tensors()
 
-# Get model details
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-height = input_details[0]['shape'][1]
-width = input_details[0]['shape'][2]
-floating_model = (input_details[0]['dtype'] == np.float32)
-
-input_mean = 127.5
-input_std = 127.5
-boxes_idx, classes_idx, scores_idx = 1, 3, 0
+# Get input shape required by the model
+_, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
 
 # Open video file
 video = cv2.VideoCapture(TEST_VIDEO_PATH)
-imW = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-imH = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+video_width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+video_height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 fps = FPS().start()
 
 while(video.isOpened()):
-    # Acquire frame and resize to expected shape [1xHxWx3]
     ret, frame = video.read()
     if not ret:
       break
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = cv2.resize(frame_rgb, (width, height))
-    input_data = np.expand_dims(frame_resized, axis=0)
+    # Preprocess frame
+    processed_frame = preprocess_frame(frame, (input_height, input_width))
 
-    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-    if floating_model:
-        input_data = (np.float32(input_data) - input_mean) / input_std
-    
-    # Perform the actual detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]['index'],input_data)
-    interpreter.invoke()
+    # Run object detection
+    results = detect(interpreter, processed_frame, MIN_CONF_THRESHOLD)
 
-    # Bounding box coordinates of detected objects
-    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0]
-    # Class index of detected objects
-    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0]
-    # Confidence of detected objects
-    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0]
-
-    # Loop over all detections and draw detection box 
-    # if confidence is above minimum threshold
-    for i in range(len(scores)):
-        if ((scores[i] > MIN_CONF_THRESHOLD) and (scores[i] <= 1.0)):
-
-            # Get bounding box coordinates and draw box
-            # Interpreter can return coordinates that are outside of image dimensions,
-            # need to force them to be within image using max() and min()
-            ymin = int(max(1,(boxes[i][0] * imH)))
-            xmin = int(max(1,(boxes[i][1] * imW)))
-            ymax = int(min(imH,(boxes[i][2] * imH)))
-            xmax = int(min(imW,(boxes[i][3] * imW)))
-            
-            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
-
-            # Draw label
-            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+    # Draw bounding boxes
+    frame = utils.visualize(frame, results, labels)
 
     # Show the FPS
     fps.update()
@@ -92,11 +83,12 @@ while(video.isOpened()):
     cv2.putText(frame, fps_text, (24, 20), cv2.FONT_HERSHEY_PLAIN,
                 1, (0, 0, 255), 1)
 
-    cv2.imshow('Test image', frame)
+    cv2.imshow('Test video', frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
 fps.stop()
 print("Tiempo aproximado por frame {}".format(fps.fps()))
+
 video.release()
 cv2.destroyAllWindows()
